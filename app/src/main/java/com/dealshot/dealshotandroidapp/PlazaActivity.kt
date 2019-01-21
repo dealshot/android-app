ec727f80358cc4eb89bead3b30ad669c8ccda01d
Xiao Xin <xinx1031@gmail.com>
2019-01-21 13:56:09 -0500

    [app] route plaza to login; add store model for errand management with mock completed.

--- app/src/main/java/com/dealshot/dealshotandroidapp/PlazaActivity.kt
+++ app/src/main/java/com/dealshot/dealshotandroidapp/PlazaActivity.kt
@@ -1,4 +1,26 @@
 package com.dealshot.dealshotandroidapp
 
-class PlazaActivity {
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dealshot.dealshotandroidapp.fragment.PlazaMainFragment
+import com.google.firebase.auth.FirebaseUser
+
+class PlazaActivity : AppCompatActivity() {
+  companion object {
+    const val ARGS_USER = "PLAZA_USER"
+  }
+
+  private var user: FirebaseUser? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_plaza)
+
+    user = intent.getParcelableExtra(ARGS_USER)
+
+    supportFragmentManager
+      .beginTransaction()
+      .replace(R.id.fragment_container, PlazaMainFragment.newInstance(user))
+      .commit()
+  }
 }
